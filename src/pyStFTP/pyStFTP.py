b8cd82b719767dcc6b34d5dbbce1e2f4ab3b48b8
mickael <Spacef0rm3>
2022-06-26 19:11:31 +0200

    Changed test

--- src/pyStFTP/pyStFTP.py
+++ src/pyStFTP/pyStFTP.py
@@ -1,64 +1,43 @@
 import paramiko
+import sys
 
 
 def create_sftp_client(host, port, username, password, keyfilepath,
                        keyfiletype):
-    """
-    create_sftp_client(host, port, username, password, keyfilepath,
-    keyfiletype) -> SFTPClient
 
-    Creates a SFTP client connected to the supplied host on the supplied
-    port authenticating as the user with
-    supplied username and supplied password or with the private key in a
-    file with the supplied path.
-    If a private key is used for authentication, the type of the keyfile
-    needs to be specified as DSA or RSA.
-    :rtype: SFTPClient object.
-    """
     sftp = None
     key = None
     transport = None
-    try:
-        if keyfilepath is not None:
-            # Get private key used to authenticate user.
-            if keyfiletype == 'DSA':
-                # The private key is a DSA type key.
-                key = paramiko.DSSKey.from_private_key_file(keyfilepath)
-            else:
-                # The private key is a RSA type key.
-                key = paramiko.RSAKey.from_private_key(keyfilepath)
+   # try:
+    if keyfilepath is not None:
+        # Get private key used to authenticate user.
+        if keyfiletype == 'DSA':
+            # The private key is a DSA type key.
+            key = paramiko.DSSKey.from_private_key_file(keyfilepath)
+        else:
+            # The private key is a RSA type key.
+            key = paramiko.RSAKey.from_private_key(keyfilepath)
 
-        # Create Transport object using supplied method of authentication.
-        transport = paramiko.Transport((host, port))
-        transport.connect(None, username, password, key)
+    # Create Transport object using supplied method of authentication.
+    transport = paramiko.Transport((host, port))
+    transport.connect(None, username, password, key)
 
-        sftp = paramiko.SFTPClient.from_transport(transport)
+    sftp = paramiko.SFTPClient.from_transport(transport)
 
-        return sftp
-    except Exception as e:
-        print('An error occurred creating SFTP client: %s: %s' % (
-            e.__class__, e))
-        if sftp is not None:
-            sftp.close()
-        if transport is not None:
-            transport.close()
-        pass
+    return sftp
+    # except Exception as e:
+    #     print('An error occurred creating SFTP client: %s: %s' % (
+    #         e.__class__, e))
+    #     if sftp is not None:
+    #         sftp.close()
+    #     if transport is not None:
+    #         transport.close()
+    #     pass
 
 
 def create_sftp_client2(host, port, username, password, keyfilepath,
                         keyfiletype):
-    """
-    create_sftp_client(host, port, username, password, keyfilepath,
-    keyfiletype) -> SFTPClient
 
-    Creates a SFTP client connected to the supplied host on the supplied
-    port authenticating as the user with
-    supplied username and supplied password or with the private key in a
-    file with the supplied path.
-    If a private key is used for authentication, the type of the keyfile
-    needs to be specified as DSA or RSA.
-    :rtype: SFTPClient object.
-    """
     ssh = None
     sftp = None
     key = None
--- test/test_pyStFTP.py
+++ test/test_pyStFTP.py
@@ -1,36 +1,30 @@
 from unittest import TestCase
 
-from src.pyStFTP.pyStFTP import create_sftp_client
+from src.pyStFTP.pyStFTP import create_sftp_client, create_sftp_client2
 
 
 class Test(TestCase):
     def test_create_sftp_client(self):
         host = 'localhost'
         port = 22
-        username = 'ivan'
-        keyfile_path = None
-        password = 'secretpassword'
+        username = 'mickael.eriksson'
+        keyfile_path = '/Users/mickael.eriksson/.ssh/id_rsa.pub'
+        password = 'Tea4ever'
 
         sftpclient = create_sftp_client(host, port, username, password,
                                         keyfile_path,
-                                        'DSA')
+                                        'RSA')
 
-        # List files in the default directory on the remote computer.
-        dirlist = sftpclient.listdir('.')
-        for row in dirlist:
-            print
-            row
+        # Download
+        filepath = "/etc/passwd"
+        localpath = "/home/remotepasswd"
+        sftpclient.get(filepath, localpath)
 
-        # Retrieve a file with the name 'remote_file.txt' on the remote
-        # computer and
-        # store it in a file named 'downloaded_file.txt'
-        # next to this SFT client program.
-        sftpclient.get('remote_file.txt', 'downloaded_file.txt')
-        # Upload a file that locally has the name 'testfile.txt' to a file
-        # on the
-        # remote computer that will have the name 'remote_testfile.txt'.
-        sftpclient.put('testfile.txt', 'remote_testfile.txt')
+        # Upload
+        filepath = "/home/foo.jpg"
+        localpath = "/home/pony.jpg"
+        sftpclient.put(localpath, filepath)
 
-        # We're done with the SFTPClient.
-        sftpclient.close()
-
+        # Close
+        if sftpclient:
+            sftpclient.close()
